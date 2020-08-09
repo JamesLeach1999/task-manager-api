c4adc4ef55297f36b4523e772c55ee751c7447ef
JamesLeach1999 <55150819+JamesLeach1999@users.noreply.github.com>
2020-08-09 16:37:42 +0100

    Removing course comments

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
 // we define the models in different files and folders to keep organised
-
+// here we essentially define the equivalent of a table in sql
 const taskSchema = new mongoose.Schema({
     description: {
         type: String,
@@ -17,45 +17,12 @@ const taskSchema = new mongoose.Schema({
         // this sets it directly on mongoose schema. every task nees ds an owner
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        // this is referencing oter models. like foreign keys. the omdel name is User
+        // this is referencing other models. like foreign keys. the model name is User. like foregin keys in SQL
         ref: "User"
     }
 }, {
-    // i have no idea why its out of the object like this. i guess its some sort of methd
     timestamps:true
 })
-// const task = mongoose.model("Task", {
-//     description: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     }, 
-//     completed: {
-//         type: Boolean,
-//         required: false,
-//         default: false
-//     },
-//     owner: {
-//         // this sets it directly on mongoose schema. every task nees ds an owner
-//         type: mongoose.Schema.Types.ObjectId,
-//         required: true,
-//         // this is referencing oter models. like foreign keys. the omdel name is User
-//         ref: "User"
-//     }
-//         // timestamps: true
-    
-// })
 
-// const t = new task({
-//     description: "hello there"
-//     // completed: true
-// })
-
-// // THIS BASICALLY TURNS IT INTO A PROMISE OBJECT
-// t.save().then((result) => {
-//     console.log(result)
-// }).catch((error) => {
-//     console.log(error)
-// })
 const Task = mongoose.model("Task", taskSchema)
 module.exports = Task;
