6c6d9a9e3f1ef0ac36c4827374eea6a1c34e3a01
JamesLeach1999 <55150819+JamesLeach1999@users.noreply.github.com>
2020-08-09 16:34:30 +0100

    Removing unnecessary course comments

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,16 +2,11 @@
 // much nicer to do it here instead of in routers
 const sgMail = require("@sendgrid/mail")
 
-// const sendGridAPIKey = "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"
+
 // still need the process.env to access environment variables
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 // surprisingly easy this. can pass in html to make it styled 
-// sgMail.send({
-//     to: "jadlljames@gmail.com",
-//     from: "jadlljames@gmail.com",
-//     subject: "first email",
-//     text: "hope it wenrt through"
-// })
+
 // send returns a promise
 const sendWelcome = (email, name) => {
     sgMail.send({
